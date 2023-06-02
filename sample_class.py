719585097950ba980a98efbec176d711de6b491e
Derek <92183446+DerekXCX@users.noreply.github.com>
2023-06-02 15:29:34 -0400

    Delete sample_class.py

--- sample_class.py
+++
@@ -1,7 +0,0 @@
-import signals
-
-class create:
-    def __init__(self):
-        self.testing_signal = signals.create()
-    def fireSignal(self):
-        self.testing_signal.Fire("Fired connection")
